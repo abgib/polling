170652118cb43945b9aeca1aa38a2b71372abd68
App Academy Student <appacademy@Apps-Mac-mini-7.local>
2015-06-11 10:01:24 -0400

    Initialize repo, create_user migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polling::Application.config.secret_key_base = '4e877aa15963007cfd7794de745eb4f521ac12d5ba55f87dfc44b9053edc2fe1cd701ce04781306969c4ad3fd367a6785b5fd2320a8f2ce397fa3dcec045ea96'
